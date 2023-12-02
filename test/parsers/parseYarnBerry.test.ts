65a0e5cb61680acf97103c2601ffd01d3254ff33
BlazingWizard <lobanov.viktor.v@gmail.com>
2023-12-02 19:58:53 +0300

    Add test on dependencies

--- test/parsers/parseYarnBerry.test.ts
+++ test/parsers/parseYarnBerry.test.ts
@@ -80,4 +80,50 @@ describe("parseYarnBerry", () => {
 		expect(packagesInfo).toHaveLength(1);
 		expect(first(packagesInfo).dependencies).toHaveLength(0);
 	});
+
+	test("Has 5 dependencies", async () => {
+		const reader = () =>
+			byLineReader(`
+				"yarn_classic@workspace:.":
+				version: 0.0.0-use.local
+				resolution: "yarn_classic@workspace:."
+				dependencies:
+					file-package: "file:file-package"
+					link-package: "link:link-package"
+					react: ^18.2.0
+					typescript: ^5.1.6
+					xlsx: "file:vendor/xlsx-0.20.0.tgz"
+				languageName: unknown
+				linkType: soft
+			`);
+		const packagesInfo = await parseYarnBerry(reader);
+		const dependencies = first(packagesInfo).dependencies;
+
+		expect(packagesInfo).toHaveLength(1);
+		expect(dependencies).toHaveLength(5);
+	});
+
+	test("Has js-tokens dependency", async () => {
+		const reader = () =>
+			byLineReader(`
+				"loose-envify@npm:^1.1.0":
+				version: 1.4.0
+				resolution: "loose-envify@npm:1.4.0"
+				dependencies:
+					js-tokens: ^3.0.0 || ^4.0.0
+				bin:
+					loose-envify: cli.js
+				checksum: 6517e24e0cad87ec9888f500c5b5947032cdfe6ef65e1c1936a0c48a524b81e65542c9c3edc91c97d5bddc806ee2a985dbc79be89215d613b1de5db6d1cfe6f4
+				languageName: node
+				linkType: hard
+			`);
+		const packagesInfo = await parseYarnBerry(reader);
+		const dependencies = first(packagesInfo).dependencies;
+
+		expect(packagesInfo).toHaveLength(1);
+		expect(dependencies).toHaveLength(1);
+
+		expect(first(dependencies).name).toBe("js-tokens");
+		expect(first(dependencies).requestedVersion).toBe("^3.0.0 || ^4.0.0");
+	});
 });
